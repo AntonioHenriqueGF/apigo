919eec24a1bce8e719814610799428ed6dad74a4
AntonioHenriqueGF <hgfantonio@gmail.com>
2024-01-28 17:06:13 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,8 +1,24 @@
 package main
 
-import "github.com/AntonioHenriqueGF/apigo/router"
+import (
+	"github.com/AntonioHenriqueGF/apigo/config"
+	"github.com/AntonioHenriqueGF/apigo/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+
+	// Initialize configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Error initializing configs: %s", err)
+		return
+	}
+
 	// Initializes the router
 	router.Init()
 }
