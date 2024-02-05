d03b7176ef1cff1b94759af098b8aefe7f434169
AntonioHenriqueGF <hgfantonio@gmail.com>
2024-02-04 22:48:02 -0300

    Add initialization for Logger and Db variables in handler package

--- handler/handler.go
+++ handler/handler.go
@@ -1,1 +1,17 @@
 package handler
+
+import (
+	"database/sql"
+
+	"github.com/AntonioHenriqueGF/apigo/config"
+)
+
+var (
+	Logger *config.Logger
+	Db     *sql.DB
+)
+
+func InitializeHandler() {
+	Logger = config.GetLogger("handler")
+	Db = config.GetDB()
+}
