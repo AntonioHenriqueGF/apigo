ca57ea9937f9caf0fbcf48305eff71822835e1df
AntonioHenriqueGF <hgfantonio@gmail.com>
2024-02-14 22:04:35 -0300

    Add success response handler

--- handler/response.go
+++ handler/response.go
@@ -1,11 +1,22 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func sendError(ctx *gin.Context, code int, msg string) {
 	ctx.JSON(code, gin.H{
 		"message":   msg,
 		"errorCode": code,
 	})
+}
 
+func sendSuccess(ctx *gin.Context, operation string) {
+	ctx.Header("Content-Type", "application/json")
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": fmt.Sprintf("Operation success: %s", operation),
+	})
 }
