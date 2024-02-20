8b16a06e28f1b60510cce1afbd0db3e8ac2e89a7
AntonioHenriqueGF <hgfantonio@gmail.com>
2024-02-19 21:59:46 -0300

    Add validation for post existence in UpdatePostHandler

--- handler/updatePost.go
+++ handler/updatePost.go
@@ -7,6 +7,28 @@ import (
 )
 
 func UpdatePostHandler(ctx *gin.Context) {
+	id := ctx.Param("id")
+	if id == "" {
+		err := errParamIsRequired("id", "urlParam")
+		logger.Errorf("validation error: %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	hasPost, err := repo.Post.ExistsByID(ctx, id)
+
+	if err != nil {
+		logger.Errorf("Error obtaining post: %s", err.Error())
+		sendError(ctx, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	if !hasPost {
+		err := errEntrieNotFound("post")
+		logger.Errorf("validation error: %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
 	ctx.JSON(http.StatusOK, gin.H{
 		"message": "PUT /post",
 	})
