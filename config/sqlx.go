b5ef80b3868278cfe69b8950d15b1d43b44ec676
AntonioHenriqueGF <hgfantonio@gmail.com>
2025-08-19 20:59:15 -0300

    Refactor database connection functions to implement retry logic with exponential backoff

--- config/sqlx.go
+++ config/sqlx.go
@@ -2,39 +2,62 @@ package config
 
 import (
 	"fmt"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 )
 
 func GetReaderSqlx() *sqlx.DB {
-
-	// Get environment variables
 	user := GetEnv("DB_USER")
 	password := GetEnv("DB_PASSWORD")
 	host := GetEnv("DB_HOST")
 	database := GetEnv("DB_DATABASE")
+	connectionString := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?allowNativePasswords=false&parseTime=true", user, password, host, database)
 
-	// Create the connection string
-	connectionString := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?allowNativePasswords=true&parseTime=true", user, password, host, database)
-
-	reader := sqlx.MustConnect("mysql", connectionString)
-
-	return reader
+	var db *sqlx.DB
+	var err error
+	maxRetries := 10
+	for i := 0; i < maxRetries; i++ {
+		db, err = sqlx.Connect("mysql", connectionString)
+		if err == nil {
+			return db
+		}
+		logger.Warningf("Tentativa %d: Falha ao conectar ao banco de dados: %v", i+1, err)
+		// Exponential backoff: 1s, 2s, 4s, ...
+		wait := 1 << i
+		if wait > 30 {
+			wait = 30
+		}
+		time.Sleep(time.Duration(wait) * time.Second)
+	}
+	logger.debug.Fatalf("Não foi possível conectar ao banco de dados após %d tentativas: %v", maxRetries, err)
+	return nil
 }
 
 func GetWriterSqlx() *sqlx.DB {
-
-	// Get environment variables
 	user := GetEnv("DB_USER")
 	password := GetEnv("DB_PASSWORD")
 	host := GetEnv("DB_HOST")
 	database := GetEnv("DB_DATABASE")
+	connectionString := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?allowNativePasswords=false&parseTime=true", user, password, host, database)
+	logger.Debug(connectionString)
 
-	// Create the connection string
-	connectionString := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?allowNativePasswords=true&parseTime=true", user, password, host, database)
-
-	writer := sqlx.MustConnect("mysql", connectionString)
-
-	return writer
+	var db *sqlx.DB
+	var err error
+	maxRetries := 10
+	for i := 0; i < maxRetries; i++ {
+		db, err = sqlx.Connect("mysql", connectionString)
+		if err == nil {
+			return db
+		}
+		logger.Warningf("Tentativa %d: Falha ao conectar ao banco de dados: %v", i+1, err)
+		wait := 1 << i
+		if wait > 30 {
+			wait = 30
+		}
+		time.Sleep(time.Duration(wait) * time.Second)
+	}
+	logger.debug.Fatalf("Não foi possível conectar ao banco de dados após %d tentativas: %v", maxRetries, err)
+	return nil
 }
